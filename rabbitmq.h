ebbf9f5554094276c83efb8fa59517c427e8b529
Andrey Kostylev <kostylevandrey113@gmail.com>
2016-08-15 02:36:43 +0300

    changed paths to headers

--- rabbitmq.h
+++ rabbitmq.h
@@ -1,4 +1,4 @@
-#import "include/amqp.h"
-#import "include/amqp_framing.h"
-#import "include/amqp_ssl_socket.h"
-#import "include/amqp_tcp_socket.h"
+#import <amqp.h>
+#import <amqp_framing.h>
+#import <amqp_ssl_socket.h>
+#import <amqp_tcp_socket.h>
