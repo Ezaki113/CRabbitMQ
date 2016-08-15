81e4b9c136e5600239b8ee4c44bc310f8700c4e5
Andrey Kostylev <kostylevandrey113@gmail.com>
2016-08-15 06:08:26 +0300

    try to hack variadic funcs

--- rabbitmq.h
+++ rabbitmq.h
@@ -2,3 +2,7 @@
 #import <amqp_framing.h>
 #import <amqp_ssl_socket.h>
 #import <amqp_tcp_socket.h>
+
+extern inline amqp_rpc_reply_t amqp_login_plain(amqp_connection_state_t state, char const *vhost, int channel_max, int frame_max, int heartbeat, const char * username, const char * password) {
+    return amqp_login(state, vhost, channel_max, frame_max, heartbeat, AMQP_SASL_METHOD_PLAIN, username, password);
+}
